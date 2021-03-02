8b73751861f88cbe0bd646375bfa0026af6c7248
Jason Washburn <wburn@wburn.net>
2021-03-02 06:26:11 -0600

    prompts user, checks for correct answer, writes to map

---
+++ main.py
@@ -0,0 +1,41 @@
+from turtle import Screen, Turtle, textinput
+import pandas as pd
+
+# Declare initial variables
+map_image_path = "./blank_states_img.gif"
+states_data_path = "./50_states.csv"
+states_correct = 0
+
+# Set-up screen
+screen = Screen()
+screen.setup(height=491, width=725)
+screen.title("US States Game")
+screen.bgpic(map_image_path)
+
+# Set-up turtle for writing
+turtle = Turtle()
+turtle.penup()
+turtle.hideturtle()
+turtle.pencolor('black')
+
+# Load state data
+states = pd.read_csv("50_states.csv")
+
+game_is_on = True
+while game_is_on:
+    user_input = textinput(f"{states_correct}/50 States Correct", "What's another state name?")
+    if type(user_input) == str:
+        user_input = user_input.lower()
+        user_input = user_input.capitalize()
+        if states.state.str.contains(user_input).any():
+            state_x = int(states[states.state == user_input].x)
+            state_y = int(states[states.state == user_input].y)
+            turtle.goto(state_x, state_y)
+            turtle.write(user_input, align='center')
+        else:
+            print(False)
+    else:
+        game_is_on = False
+
+
+screen.exitonclick()
